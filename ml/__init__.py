39b68bd69351241aa0a5966f3a0105f4ae38e4cc
Marat-Ryskulov <76579313+Marat-Ryskulov@users.noreply.github.com>
2025-06-09 20:08:48 +0600

    Add files via upload

---
+++ ml/__init__.py
@@ -0,0 +1,6 @@
+# ml/__init__.py - Очищенная версия
+from .feature_extractor import FeatureExtractor
+from .model_manager import ModelManager
+from .knn_trainer import KNNTrainer
+
+__all__ = ['FeatureExtractor', 'ModelManager', 'KNNTrainer']
